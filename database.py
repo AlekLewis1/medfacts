ec92a3844eb7ce16a3a6c518d3bd201a684e4950
AlekLewis1 <alekseilewis@gmail.com>
2025-05-23 05:27:24 -0400

    Update database.py
    first database py draft

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
+# database.py
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# URL that connects to your SQL database
+#update with username, password, and name
+DATABASE_URL = "postgresql://username:password@localhost/medexplain"
+
+# database engine
+engine = create_engine(DATABASE_URL) 
+
+# database session
+SessionLcal + sessionmaker(autocommit=False, autoflush=False, autoflush=False, bind=engine) 
+
+# base class for tables
+Base = declarative_base()
