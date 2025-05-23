7df994d377615d056cbb93ef98a2dbfb18e4bd26
AlekLewis1 <alekseilewis@gmail.com>
2025-05-23 05:47:33 -0400

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 # URL that connects to your SQL database
 #update with username, password, and name
-DATABASE_URL = "postgresql://username:password@localhost/medexplain"
+DATABASE_URL = "postgresql://username:password@localhost/medefacts"
 
 # database engine
 engine = create_engine(DATABASE_URL) 
